04ae5e4aecaddd54aa433682daa9251458db977e
joseluistapia16 <joseluistapia09@gmail.com>
2024-11-27 15:17:27 -0500

    ACTUALIZACION PROYECTO 3B 27/11/2024 15:15PM

--- src/basedatos3b/BaseDatos3B.java
+++ src/basedatos3b/BaseDatos3B.java
@@ -1,5 +1,11 @@
 package basedatos3b;
 
+/*
+    Autor:  Ing. Jose Luis Tapia
+    Fecha: 27-11-2024  Hora: 15:16pm
+*/
+
+
 public class BaseDatos3B {
 
     public static void main(String[] args) {
